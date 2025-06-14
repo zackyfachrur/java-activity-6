51e62d5d1cf56587d5d2f06c9f763533097522f9
zackster <mochzackyaja@gmail.com>
2025-06-15 00:47:46 +0700

    Create 6-2.java

---
+++ 6-2.java
@@ -0,0 +1,8 @@
+// Tuliskan definisi method average di bawah.
+ public static void average(double angka1, double angka2, double angka3) {
+        double jumlah = angka1 + angka2 + angka3;
+
+        double hasilRataRata = jumlah / 3.0;
+
+        System.out.printf("%.2f", hasilRataRata);
+}
